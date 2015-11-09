4b0caf09b10ce3757a5b7faaca2ae37d38292fa3
Aditya Bharadwaj <bharadwaj.adi18@gmail.com>
2015-11-09 17:35:18 -0500

    updating plots

--- PlotResults.py
+++ PlotResults.py
@@ -6,7 +6,7 @@ from Ensemble import Ensemble
 __author__ = 'adb'
 
 
-datafile = 'sephone'
+datafile = 'seprox'
 results_folder = 'hyperedge-results/' + datafile
 graph_summary_file = 'hyperedge-results/' + datafile + '/' + datafile + '.summary'
 lam_results_folder = results_folder + '/lam'
@@ -32,11 +32,12 @@ def plot_ssd_vs_rank(max_rank):
     with open(lam_results_folder + '/top-' + str(max_rank) + '-hyperedges.tsv', 'r') as f:
         lam_ssds = [line.split('\t')[-1] for line in f]
 
-    plt.subplot(211)
-    plt.plot([i for i in range(1, len(am_ssds)+1)], am_ssds, label='am')
-    plt.plot([i for i in range(1, len(lam_ssds)+1)], lam_ssds, label='lam')
+    # plt.subplot(211)
+    plt.plot([i for i in range(1, len(am_ssds)+1)], am_ssds, label='ANTI-MONOTONE')
+    plt.plot([i for i in range(1, len(lam_ssds)+1)], lam_ssds, label='LOOSELY ANTI-MONOTONE')
     plt.legend(loc='lower right')
     plt.xlabel('UC Rank')
+    # plt.xscale('log')
     plt.ylabel('SSD')
 
 
@@ -49,11 +50,12 @@ def plot_percent_of_nodes_vs_rank(max_rank, num_of_nodes):
         lam_nodes = [line.split('\t')[:-1] for line in f]
         lam_percentage_of_nodes_covered = compute_percentage_of_nodes_covered(lam_nodes, num_of_nodes)
 
-    plt.subplot(212)
-    plt.plot([i for i in range(1, len(am_percentage_of_nodes_covered)+1)], am_percentage_of_nodes_covered, label='am')
-    plt.plot([i for i in range(1, len(lam_percentage_of_nodes_covered)+1)], lam_percentage_of_nodes_covered, label='lam')
+    # plt.subplot(212)
+    plt.plot([i for i in range(1, len(am_percentage_of_nodes_covered)+1)], am_percentage_of_nodes_covered, label='ANTI-MONOTONE')
+    plt.plot([i for i in range(1, len(lam_percentage_of_nodes_covered)+1)], lam_percentage_of_nodes_covered, label='LOOSELY ANTI-MONOTONE')
     plt.legend(loc='lower right')
     plt.xlabel('UC Rank')
+    # plt.xscale('log')
     plt.ylabel('% Nodes')
 
 def plot_ssd_of_uncovered_nodes_vs_rank(max_rank, T):
@@ -144,10 +146,10 @@ with open(datafile, 'r') as f:
         else:
             T.add_node(int(v1))
 
-# plot_ssd_vs_rank(1024)
+plot_ssd_vs_rank(128)
 # plot_percent_of_nodes_vs_rank(1024, num_of_nodes)
 # plot_ssd_of_uncovered_nodes_vs_rank(2048, T)
 # print(plot_num_of_k_size_ucs_vs_ssd_cutoff_for_am([0.1, 0.2, 0.3, 0.4, 0.5, 0.6]))
-print(plot_num_of_k_size_ucs_vs_ssd_cutoff_for_lam([0.1, 0.2, 0.3, 0.4,  0.5, 0.6]))
+# print(plot_num_of_k_size_ucs_vs_ssd_cutoff_for_lam([0.1, 0.2, 0.3, 0.4,  0.5, 0.6]))
 
 plt.show()
